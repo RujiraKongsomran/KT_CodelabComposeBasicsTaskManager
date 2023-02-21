42683d9303cf6251ea3b1f523888d722837f6bb3
RujiraKongsomran <rujirabiwberry@gmail.com>
2023-02-21 17:12:56 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_codelabcomposebasicstaskmanager/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_codelabcomposebasicstaskmanager/MainActivity.kt
@@ -3,13 +3,22 @@ package com.rujirakongsomran.kt_codelabcomposebasicstaskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rujirakongsomran.kt_codelabcomposebasicstaskmanager.ui.theme.KT_CodelabComposeBasicsTaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +27,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             KT_CodelabComposeBasicsTaskManagerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface {
+                    TaskManagerApp()
                 }
             }
         }
@@ -30,17 +36,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskManagerApp() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+        Text(
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     KT_CodelabComposeBasicsTaskManagerTheme {
-        Greeting("Android")
+        TaskManagerApp()
     }
 }
